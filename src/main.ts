b4689961b5f6330edfb2ad01b13e742b2d042f3d
Paul Watts <paulcwatts@gmail.com>
2022-12-08 15:37:16 -0800

    Add urql
    Version 1.0.3, says it's missing typing.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createClient } from '@urql/vue';
 
 import './assets/main.css'
 
+const client = createClient({
+    url: 'http://localhost:3000/graphql',
+});
 createApp(App).mount('#app')
